51bb835899e53741f76d7657d67b6171b8fb461a
Abhigyan Khaund <160063176+DevAbhi99@users.noreply.github.com>
2025-01-30 12:16:07 +0000

    pushing arduino tutorial code

---
+++ arduinofunctions.cpp
@@ -0,0 +1,28 @@
+//Arduino Inbuilt functions
+
+
+//void setup() :- Code gets executed only once
+
+//void loop() :- Code gets executed in a loop
+
+
+//digitalRead(pin) :- Reads digital data from the pin
+
+//digitalWrite(pin, mode) :- Provides Digital output in two modes (HIGH,LOW)
+
+//pinMode(pin, mode) :- sets up the pin usually written within the void setup loop. mode are INPUT/OUTPUT
+
+//analogRead(pin) :- reads analog data
+
+//analogWrite(pin, value):- It is ued to controll analog signals as the value specified. Used for pulse width modulation
+
+//Serial.begin(9600) :- 9600 is baud unit or data rate in bits per second(when communication happens serially between computer and arduino). This command starts the serial communication
+
+//Serial.available() :- After serial communication is initialized. how many times the serial communication is established
+
+//Serial.read() :- takes user input. usually takes char value but can be converted to int using number-'0'
+
+//Serial.println():- It is used to print values in serial monitor
+
+
+//remaining all the code syntaxes are same as java
